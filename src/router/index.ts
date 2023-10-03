9909490b97857e7fe80ed5edf2329c29a73fb70d
A-Moualla <ammar13a2r@gmail.com>
2023-10-03 11:00:49 +0300

    uploading project to GitHub

---
+++ src/router/index.ts
@@ -0,0 +1,167 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+import type { App } from 'vue'
+import { Layout, getParentLayout } from '@/utils/routerHelper'
+import { useI18n } from '@/hooks/web/useI18n'
+
+const { t } = useI18n()
+
+export const constantRouterMap: AppRouteRecordRaw[] = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    name: 'Root',
+    meta: {
+      hidden: true
+    }
+  },
+  {
+    path: '/redirect',
+    component: Layout,
+    name: 'Redirect',
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        name: 'Redirect',
+        component: () => import('@/views/Redirect/Redirect.vue'),
+        meta: {}
+      }
+    ],
+    meta: {
+      hidden: true,
+      noTagsView: true
+    }
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/Login/Login.vue'),
+    name: 'Login',
+    meta: {
+      hidden: true,
+      title: t('router.login'),
+      noTagsView: true
+    }
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/Error/404.vue'),
+    name: 'NoFind',
+    meta: {
+      hidden: true,
+      title: '404',
+      noTagsView: true
+    }
+  }
+]
+
+export const asyncRouterMap: AppRouteRecordRaw[] = [
+  {
+    path: '/home',
+    component: Layout,
+    redirect: '/home/welcome',
+    name: 'Home',
+    meta: {
+      title: t('router.home')
+    },
+    children: [
+      {
+        path: 'welcome',
+        name: 'Welcome',
+        component: () => import('@/views/Extra/Welcome.vue'),
+        meta: {
+          title: t('router.home'),
+          icon: 'carbon:home'
+        }
+      }
+    ]
+  },
+  {
+    path: '/level',
+    component: Layout,
+    name: 'Level',
+    meta: {
+      title: t('router.level'),
+      icon: 'carbon:db2-database'
+    },
+    children: [
+      {
+        path: 'menu1',
+        name: 'Menu1',
+        component: getParentLayout(),
+        meta: {
+          title: t('router.menu1')
+        },
+        children: [
+          {
+            path: 'menu1-1',
+            name: 'Menu11',
+            component: () => import('@/views/Level/Menu111.vue'),
+            meta: {
+              title: t('router.menu11')
+            }
+          },
+          {
+            path: 'menu1-2',
+            name: 'Menu12',
+            component: () => import('@/views/Level/Menu12.vue'),
+            meta: {
+              title: t('router.menu12')
+            }
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        name: 'Menu2',
+        component: () => import('@/views/Level/Menu2.vue'),
+        meta: {
+          title: t('router.menu2')
+        }
+      }
+    ]
+  },
+  {
+    path: '/about',
+    component: Layout,
+    redirect: '/about/me',
+    name: 'About',
+    meta: {
+      title: t('router.about')
+    },
+    children: [
+      {
+        path: 'me',
+        name: 'Me',
+        component: () => import('@/views/Extra/About.vue'),
+        meta: {
+          title: t('router.about'),
+          icon: 'carbon:user-filled'
+        }
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  strict: true,
+  routes: constantRouterMap as RouteRecordRaw[],
+  scrollBehavior: () => ({ left: 0, top: 0 })
+})
+
+export const resetRouter = (): void => {
+  const resetWhiteNameList = ['Redirect', 'Login', 'NoFind', 'Root']
+  router.getRoutes().forEach((route) => {
+    const { name } = route
+    if (name && !resetWhiteNameList.includes(name as string)) {
+      router.hasRoute(name) && router.removeRoute(name)
+    }
+  })
+}
+
+export const setupRouter = (app: App<Element>) => {
+  app.use(router)
+}
+
+export default router
---
+++ src/views/Extra/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ElInput } from 'element-plus'
+import { ContentWrap } from '@/components/ContentWrap'
+import { useI18n } from '@/hooks/web/useI18n'
+import { ref } from 'vue'
+
+defineOptions({
+  name: 'Menu2'
+})
+
+const { t } = useI18n()
+
+const text = ref('')
+</script>
+
+<template>
+  <ContentWrap>
+    <div class="flex items-center">{{ t('levelDemo.welcome') }}</div>
+  </ContentWrap>
+</template>
